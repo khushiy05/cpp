aa595f2ba11ae6b7f36508c40c5bdb2ea199b2a5
khushiy05 <khushiyewale150@gmail.com>
2025-08-18 12:34:31 +0530

    header

---
+++ tut6.cpp
@@ -0,0 +1,9 @@
+//there are two types if header files
+// 1. systen header files: it comes with compiler 
+#include <iostream>
+// 2. user defined header files: it is written by the programer
+// #include "this.h" //--> this will produce an error if this.h is not present in the current directory 
+using namespace std;
+int main() {
+
+}
