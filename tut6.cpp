2a673568beff2cbdfb89c87e51729cd4fad690c4
khushiy05 <khushiyewale150@gmail.com>
2025-08-18 13:08:55 +0530

    operators

--- tut6.cpp
+++ tut6.cpp
@@ -4,6 +4,37 @@
 // 2. user defined header files: it is written by the programer
 // #include "this.h" //--> this will produce an error if this.h is not present in the current directory 
 using namespace std;
-int main() {
 
+int main() {
+    int a=4, b=5;
+    cout<<"operator in cpp:"<<endl;
+    cout<<"following are the operator in cpp:"<<endl;
+    //Arithmatic operators
+    cout << "The value of a + b is " <<a + b<< endl;
+    cout << "The value of a - b is " <<a - b<< endl;
+    cout << "The value of a * b is " <<a * b<< endl;
+    cout << "The value of a / b is " <<a / b<< endl;
+    cout << "The value of a % b is " <<a % b<< endl;
+    cout << "The value of a++ is " <<a++<< endl; // this are the increment and decrement operator   // print then increment
+    cout << "The value of a-- is " <<a--<< endl; // this are the increment and decrement operator
+    cout << "The value of ++a is " <<++a<< endl; // this are the increment and decrement operator   // increment then print
+    cout << "The value of --a is " <<--a<< endl; // this are the increment and decrement operator
+    cout<<endl; 
+
+
+    // Assignment operator --> used to assign value to variables
+    // int a=3, b=9;
+    // char d= 'd';
+
+    // Comparison operators
+    cout<<"following are the comaprison operator in cpp"<<endl;
+    cout<<"The value of a == b is "<<(a==b)<<endl;
+    cout<<"The value of a != b is "<<(a!=b)<<endl;
+    cout<<"The value of a >= b is "<<(a>=b)<<endl;
+    cout<<"The value of a <= b is "<<(a<=b)<<endl;
+    cout<<"The value of a > b is "<<(a>b)<<endl;
+    cout<<"The value of a < b is "<<(a<b)<<endl;
+
+    // logical operators
+    return 0;
 }
