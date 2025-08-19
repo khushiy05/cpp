5de5aee1029f2ec9e29aa06745ef9ee0915dd0fb
khushiy05 <khushiyewale150@gmail.com>
2025-08-19 22:26:21 +0530

    resolution operator

--- tut7.cpp
+++ tut7.cpp
@@ -1,5 +1,20 @@
 #include<iostream>>
 using namespace std;
+int c= 45;
 int main(){
+    int a, b, c;
+    cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+    c = a+b;
+    cout<<"The sum is "<<c<<endl;
+    cout<<"The global c is"<<c;
+
+
+    float d= 34.4;
+    long double e = 34.4;
     
+    cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
+    return 0;
 }
