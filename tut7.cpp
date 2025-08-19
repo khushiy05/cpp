a56d1a20b6551cc88ee5a634ccc31107369fd1d9
khushiy05 <khushiyewale150@gmail.com>
2025-08-19 22:51:39 +0530

    reference variables

--- tut7.cpp
+++ tut7.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int c= 45;
 int main(){
+
+//********Built in data types********
     int a, b, c;
     cout<<"Enter the value of a:"<<endl;
     cin>>a;
@@ -11,7 +13,7 @@ int main(){
     cout<<"The sum is "<<c<<endl;
     cout<<"The global c is"<<c;
 
-
+//********float, double and long  double Literals********
     float d= 34.4f;
     long double e = 34.4l;
     cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
@@ -19,7 +21,13 @@ int main(){
     cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
     cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
     cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
-    
     cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
+
+    //********Refernece variables********
+    float x = 455;
+    float & y = x;
+    cout<<x<<endl;
+    cout<<y<<endl;
+
     return 0;
 }
