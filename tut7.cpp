a2e7bdc7df2d4adf8c8f85013e7415699beb5010
khushiy05 <khushiyewale150@gmail.com>
2025-08-19 22:40:44 +0530

    size of opertor

--- tut7.cpp
+++ tut7.cpp
@@ -12,8 +12,13 @@ int main(){
     cout<<"The global c is"<<c;
 
 
-    float d= 34.4;
-    long double e = 34.4;
+    float d= 34.4f;
+    long double e = 34.4l;
+    cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+    cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
+    cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
     
     cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
     return 0;
