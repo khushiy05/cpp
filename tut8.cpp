6c666fed5014fbcd8edd8646f03f0ee6e5b9097d
khushiy05 <khushiyewale150@gmail.com>
2025-08-20 23:12:50 +0530

    constants

---
+++ tut8.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+    //int a = 34;
+    //cout<<"The value of a was: "<<a;
+    //a = 45;
+    //cout<<"The value of a is: "<<a;
+
+    //constants in cpp
+    //const float a = 3;
+    //cout<<"The value of a was: "<<a<<endl;
+    //a = 45; // you will get ans error because a is a constant
+    //cout<<"The value of a is: "<<a<<endl;
+
+    
+    
+
+    return 0;
+}
