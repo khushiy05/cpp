998f3df684d766abd8d63532af8f0be70ef63cfd
khushiy05 <khushiyewale150@gmail.com>
2025-08-20 23:32:59 +0530

    operator precedure

--- tut8.cpp
+++ tut8.cpp
@@ -15,15 +15,17 @@ int main(){
     //cout<<"The value of a is: "<<a<<endl;
 
     //Manipulator in cpp
-    int a = 3, b =78, c = 1233;
-    cout<<"The value of a without setw is: "<<a<<endl;
-    cout<<"The value of b without setw is: "<<b<<endl;
-    cout<<"The value of c without setw is: "<<c<<endl;
-
-    cout<<"The value of a  is: "<<setw(4)<<a<<endl;
-    cout<<"The value of b  is: "<<setw(4)<<b<<endl;
-    cout<<"The value of c  is: "<<setw(4)<<c<<endl;
-    
-
+    //int a = 3, b =78, c = 1233;
+    //cout<<"The value of a without setw is: "<<a<<endl;
+    //cout<<"The value of b without setw is: "<<b<<endl;
+    //cout<<"The value of c without setw is: "<<c<<endl;
+    //cout<<"The value of a  is: "<<setw(4)<<a<<endl;
+    //cout<<"The value of b  is: "<<setw(4)<<b<<endl;
+    //cout<<"The value of c  is: "<<setw(4)<<c<<endl;
+     
+    //operator precedure
+    int a = 3, b = 4;
+    int c = ((((a * 5) + b) - 45) + 87); // evaluated left to right
+    cout << c; // prints 61
     return 0;
 }
