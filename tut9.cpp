d78e886b2219be8a90ea265299c50edf9e961bdc
khushiy05 <khushiyewale150@gmail.com>
2025-08-22 00:27:14 +0530

    else

--- tut9.cpp
+++ tut9.cpp
@@ -5,11 +5,14 @@ int main (){
     int age;
     cout<<"Tell me your age"<<endl;
     cin>>age;
-    if(age<18){
+    if((age<18) && (age>0)){
         cout<<"You can not come to my party"<<endl;
     }
     else if(age==18){
         cout<<"tou are a kid and you will get a kid pass to the party"<<endl;
+    }
+    else if(age<1){
+        cout<<"you are not yet born"<<endl;
     }
     else{
         cout<<"you can come to my party"<<endl;
