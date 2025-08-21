745cbc230d013cf37d6473a5c36c2637da42b335
khushiy05 <khushiyewale150@gmail.com>
2025-08-22 00:37:07 +0530

    switch cases

--- tut9.cpp
+++ tut9.cpp
@@ -5,18 +5,37 @@ int main (){
     int age;
     cout<<"Tell me your age"<<endl;
     cin>>age;
-    if((age<18) && (age>0)){
-        cout<<"You can not come to my party"<<endl;
-    }
-    else if(age==18){
-        cout<<"tou are a kid and you will get a kid pass to the party"<<endl;
-    }
-    else if(age<1){
-        cout<<"you are not yet born"<<endl;
-    }
-    else{
-        cout<<"you can come to my party"<<endl;
-    }
-       
+
+    //selection control structure: If else-if else ladder
+    //if((age<18) && (age>0)){
+        //cout<<"You can not come to my party"<<endl;
+    //}
+    //else if(age==18){
+        //cout<<"tou are a kid and you will get a kid pass to the party"<<endl;
+    //}
+    //else if(age<1){
+        //cout<<"you are not yet born"<<endl;
+    //}
+    //else{
+        //cout<<"you can come to my party"<<endl;
+    //} 
+
+        //selection control structure: switch case statement
+        switch (age)
+        {
+        case 18:
+            cout<<"you are 18"<<endl;
+            break;
+        case 22:
+            cout<<"you are 22"<<endl;
+            break;
+        case 2:
+            cout<<"you are 2"<<endl;
+            break;
+        
+        default:
+        cout<<"No special cases"<<endl;
+            break;
+        }
     return 0;
 }
