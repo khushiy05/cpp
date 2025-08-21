edfd3444fa2e18453fe08091e7c7e8c3f5ac6caa
khushiy05 <khushiyewale150@gmail.com>
2025-08-21 23:55:34 +0530

    if-else

---
+++ tut9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+    //cout<<"This is tutorial 9"
+    int age;
+    cout<<"Tell me your age"<<endl;
+    cin>>age;
+    if(age<18){
+        cout<<"You can not come to my party"<<endl;
+    }
+    else if(age==18){
+        cout<<"tou are a kid and you will get a kid pass to the party"<<endl;
+    }
+    else{
+        cout<<"you can come to my party"<<endl;
+    }
+       
+    return 0;
+}
