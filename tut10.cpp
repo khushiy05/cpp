dea39ab38f5149cffa22b433359a4384459e8cbc
khushiy05 <khushiyewale150@gmail.com>
2025-08-22 00:54:22 +0530

    for-loop

---
+++ tut10.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*Loops in cpp:
+    //there are three types of loops in cpp:
+      1 - For loop - block of statement 
+      2 - while loop
+      3 - do-which loop
+    */
+
+    /*For loop in cpp*/
+    //int i = 1;
+    //cout<<i;
+    //i++; 
+
+    // syntax for for loop
+    //for(initialization; condition; updation)
+    //{
+        //loop body(cpp code);
+    //}
+    //for (int i = 0; i < 40; i++)
+    //{
+        //cout<<i<<endl;
+        //i++;
+    //}
+    
+    return 0;
+
+}
