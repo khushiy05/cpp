7f53cb584dbd8d83ca32ccb3511bd728e604f389
khushiy05 <khushiyewale150@gmail.com>
2025-08-23 20:45:37 +0530

    continue and boilerplate

--- tut11.cpp
+++ tut11.cpp
@@ -1,14 +1,23 @@
 #include<iostream>;
 using namespace std;
 int main(){
-    for (int i = 0; i < 4; i++)
+    /*for (int i = 0; i < 4; i++)
     {
 
         if(i==2){
             break; // break; - it exits from loop the output will be 0,1,2 if the "cout<<i<<endl;" will be there 
         }
         cout<<i<<endl; // the output will be 0 and 1 because of the break; 
+    }*/
+
+    for (int i = 0; i <40; i++)
+    {
+        if(i==2){
+            continue; //abhi jo run kr rhe ho usko yahe pe rok do aur next itration pe jao
+        }
+        cout<<i<<endl;
     }
+    
     
     return 0;
 }
