04d4ccb3a2c164d171b7d72ec575db2642617fc8
khushiy05 <khushiyewale150@gmail.com>
2025-08-24 21:02:17 +0530

    pointer to pointer

--- tut12.cpp
+++ tut12.cpp
@@ -10,7 +10,14 @@ int main(){
     cout<<"the address of a is"<<a<<endl;
 
     //"*" (value at) dereference operator
-    cout<<"the value at address b is "<<*b<<endl;
+    cout<<"the value at address b is "<<*b<<endl; 
+
+    // pointer to pointer ----> pointer ke bhi address ko store krta hai
+    int** c =&b;
+    cout<<"the address of b is "<<&b<<endl;
+    cout<<"the address of b is "<<c<<endl;
+    cout<<"the value at address c is "<<*c<<endl;
+    cout<<"the value at address value_at(value_at(c)) is "<<**c<<endl;
 
     return 0;
 }
